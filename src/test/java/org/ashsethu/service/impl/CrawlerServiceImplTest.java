976299ccd303b94616e7be416ffa085b0b6d14f0
aks37 <aks37@ilch-ml0381.local>
2020-04-13 08:49:42 -0500

    modified tests

--- src/test/java/org/ashsethu/service/impl/CrawlerServiceImplTest.java
+++ src/test/java/org/ashsethu/service/impl/CrawlerServiceImplTest.java
@@ -17,6 +17,7 @@ import static org.mockito.Mockito.*;
 
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 //import org.powermock.core.classloader.annotations.PrepareForTest;
 //import org.powermock.modules.junit4.PowerMockRunner;
@@ -50,7 +51,7 @@ class CrawlerServiceImplTest{
 
 
     @Test
-    void crawlTheWeb_Max_Depth_Test() throws IOException {
+    void crawlTheWeb_Page_Count_Test() throws IOException {
 
         when(mcfg.isAllowExternal()).thenReturn(true);
         when(mcfg.getMaxDepth()).thenReturn(1);
@@ -62,77 +63,55 @@ class CrawlerServiceImplTest{
 
         assertNotNull(pageRepository);
 
-        assertEquals(2, pageRepository.getPageListCount());
+        assertEquals(3, pageRepository.getPageListCount()); //3 pages should be returned as that is the list provided by mock
 
     }
 
     private Elements createElementList(){
-        Element el1 = new Element("html").attr("href", "http://linkedin.com");
-        Element el2 = new Element("html").attr("href", "http://twitter.com");
+        Element el1 = new Element("a").attr("href", "http://linkedin.com");
+        Element el2 = new Element("a").attr("href", "http://twitter.com");
+        Element el3 = new Element("a").attr("href", "http://google.com");
         Elements els = new Elements();
         els.add(el1);
         els.add(el2);
+        els.add(el3);
         return els;
     }
 
     @Test
     void crawlTheWeb_MaxDepth_Test() throws IOException {
-
-        cfg.setAllowExternal(true);
-        cfg.setMaxDepth(1);
+        when(mcfg.isAllowExternal()).thenReturn(true);
+        when(mcfg.getMaxDepth()).thenReturn(1);
+        when(mcfg.getMaxPages()).thenReturn(10);
+        when(mhtmlParser.getLinks(Mockito.any())).thenReturn(createElementList());
         PageRepository pageRepository = csil.crawlTheWeb("http://wiprodigital.com", "wiprodigital.com");
-        ArrayList<String> pages = pageRepository.getAllPages();
-        for (String record : pages) {
-
-            String links[] = record.split(Constants.KEY_SEPARATOR);
-            int actualDepth = links.length - 1; //reducing 1 because first part is base domain
-
-            if (actualDepth > 1) {
-                assert (false); //if length of array exceeds 1 then depth has exceeded 1. Fail the test
-                return;
-            }
-        }
-        assert (true);//if not asserted false, then assert true
+        verify(mhtmlParser, times(1)).getLinks(Mockito.any()); //get links should be called only once as depth has been set to 1
     }
 
     @Test
     void crawlTheWeb_MaxCount_Test() throws IOException {
-        cfg.setAllowExternal(true);
-        cfg.setMaxPages(10);
+        int maxPageCount = 2;
+        when(mcfg.isAllowExternal()).thenReturn(true);
+        when(mcfg.getMaxDepth()).thenReturn(1);
+        when(mcfg.getMaxPages()).thenReturn(maxPageCount);
+        when(mhtmlParser.getLinks(Mockito.any())).thenReturn(createElementList());
         PageRepository pageRepository = csil.crawlTheWeb("http://wiprodigital.com", "wiprodigital.com");
         ArrayList<String> pages = pageRepository.getAllPages();
-
-        if (pages.size() > 10) {
-            assert (false); //if length of array exceeds 1 then depth has exceeded 1. Fail the test
-            return;
-        }
-
-        assert (true);//if not asserted false, then assert true
+        assertEquals(maxPageCount, pages.size()); //returned pages count should be equal to the max count set initially
     }
 
 
     @Test
     void crawlTheWeb_NotAllowExternal_Test() throws IOException {
-        cfg.setAllowExternal(false);
-        cfg.setMaxPages(1000);
-        cfg.setMaxDepth(1);
+        when(mcfg.isAllowExternal()).thenReturn(false);
+        when(mcfg.getMaxDepth()).thenReturn(2);
+        when(mcfg.getMaxPages()).thenReturn(10);
+        when(mhtmlParser.getLinks(Mockito.any())).thenReturn(createElementList());
 
-        PageRepository pageRepository = csil.crawlTheWeb("https://asethura.wixsite.com/techfortech", "asethura.wixsite.com");
-        ArrayList<String> pages = pageRepository.getAllPages();
+        PageRepository pageRepository = csil.crawlTheWeb("http://wiprodigital.com", "wiprodigital.com");
 
-        for (String record : pages) {
-
-            String[] records = record.split(Constants.KEY_SEPARATOR);
-
-            String lastButOneRecord = records[records.length - 2];
-
-            if (!lastButOneRecord.contains("asethura.wixsite.com")){
-                assert(false);
-                return;
-            }
+        verify(mhtmlParser, times(1)).getLinks("http://wiprodigital.com"); //There should be only 1 url that is starting page because all returned ones are externral
         }
-        assert (true);//if not asserted false, then assert true
-    }
 
 
 }
--- src/test/java/org/ashsethu/utils/HtmlParserTest.java
+++ src/test/java/org/ashsethu/utils/HtmlParserTest.java
@@ -1,29 +1,175 @@
 package org.ashsethu.utils;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.ashsethu.service.impl.CrawlerServiceImpl;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
-@RunWith( SpringRunner.class )
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class HtmlParserTest {
 
+    protected static final Logger logger = LoggerFactory.getLogger(CrawlerServiceImpl.class);
+
     @Test
     public void noLinks_Test() throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+        server.createContext("/test", new MyHttpHandler());
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+
+        logger.info(" Server started on port 8001");
+
         HtmlParser htmlParser = new HtmlParser();
         //Testing a site with no links
-        Elements pageLinks = htmlParser.getLinks("http://www.brainjar.com/java/host/test.html");
-        assert(pageLinks.isEmpty());
+        Elements pageLinks = htmlParser.getLinks("http://localhost:8001/test");
+
+        assert (pageLinks.isEmpty());
+        server.stop(0);
     }
 
     @Test
-    public void MultiLink_Test() throws IOException {
+    public void MultiLink_Test() throws IOException, InterruptedException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+        server.createContext("/test", new MyHttpHandlerWthTwoLinks());
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+
+        logger.info(" Server started on port 8001");
+
+
         HtmlParser htmlParser = new HtmlParser();
-        Elements pageLinks = htmlParser.getLinks("https://asethura.wixsite.com/techfortech");
-        assert(pageLinks.size()==14);
+        //Elements pageLinks = htmlParser.getLinks("https://asethura.wixsite.com/techfortech");
+        Elements pageLinks = htmlParser.getLinks("http://localhost:8001/test");
+
+        logger.info(" pageLinks.size() " + pageLinks.size());
+        assert (pageLinks.size() == 2);
+        server.stop(0);
+    }
+
+    private Elements createElementList() {
+        Element el1 = new Element("a").attr("href", "http://linkedin.com");
+        Element el2 = new Element("a").attr("href", "http://twitter.com");
+        Element el3 = new Element("a").attr("href", "http://google.com");
+        Elements els = new Elements();
+        els.add(el1);
+        els.add(el2);
+        els.add(el3);
+        return els;
+    }
+
+    private class MyHttpHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String requestParamValue = null;
+
+            if ("CONNECT".equals(httpExchange.getRequestMethod())) {
+
+                requestParamValue = handleGetRequest(httpExchange);
+
+            }
+            handleResponse(httpExchange, requestParamValue);
+        }
+
+        private String handleGetRequest(HttpExchange httpExchange) {
+            return httpExchange.
+                    getRequestURI()
+                    .toString()
+                    .split("\\?")[1]
+                    .split("=")[1];
+        }
+
+        private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
+            OutputStream outputStream = httpExchange.getResponseBody();
+            StringBuilder htmlBuilder = new StringBuilder();
+            htmlBuilder.append("<html>").
+                    append("<body>").
+                    append("<h1>").
+                    append("Hello ")
+                    .append(requestParamValue)
+                    .append("</h1>")
+                    .append("</body>")
+                    .append("</html>");
+            String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+            httpExchange.sendResponseHeaders(200, htmlResponse.length());
+            outputStream.write(htmlResponse.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        }
+    }
+
+    private class MyHttpHandlerWthTwoLinks implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String requestParamValue = null;
+
+            if ("CONNECT".equals(httpExchange.getRequestMethod())) {
+
+                requestParamValue = handleGetRequest(httpExchange);
+
+            }
+            handleResponse(httpExchange, requestParamValue);
+        }
+
+        private String handleGetRequest(HttpExchange httpExchange) {
+            return httpExchange.
+                    getRequestURI()
+                    .toString()
+                    .split("\\?")[1]
+                    .split("=")[1];
+        }
+
+        private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
+            OutputStream outputStream = httpExchange.getResponseBody();
+            StringBuilder htmlBuilder = new StringBuilder();
+            htmlBuilder.append("<html>").
+                    append("<body>").
+                    append("<h1>").
+                    append("Hello ")
+                    .append(requestParamValue)
+                    .append("</h1>")
+                    .append("<a href=\"#\">").
+                    append("Level 1")
+                    .append("</a>")
+                    .append("<a href=\"#\">").
+                    append("Level 2")
+                    .append("</a>")
+                    .append("</body>")
+                    .append("</html>");
+            //String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+            String htmlResponse = htmlBuilder.toString();
+            logger.info(htmlResponse);
+            httpExchange.sendResponseHeaders(200, htmlResponse.length());
+            outputStream.write(htmlResponse.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        }
     }
 }
+
+
